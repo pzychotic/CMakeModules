c2dd930c139f8d68836131dba69fc895533e9ae4
Thomas Roß <pzycho1980@gmail.com>
2015-10-04 12:51:36 +0200

    Simplified compiler detection.

--- PzyPlatform.cmake
+++ PzyPlatform.cmake
@@ -11,43 +11,23 @@ set( PZY_COMPILER_VERSION   "Unknown" )
 set( PZY_ARCH               0 )
 
 
+# find the activated programming language, don't care for fortran
+if( DEFINED CMAKE_CXX_COMPILER_ID )
+    set( LANGUAGE "CXX" )
+elseif( DEFINED CMAKE_C_COMPILER_ID )
+    set( LANGUAGE "C" )
+endif()
+
 # STREQUAL doesn't work here because MSVC is a variable that gets dereferenced
-if( CMAKE_C_COMPILER_ID AND CMAKE_C_COMPILER_ID MATCHES "MSVC" )
+if( DEFINED CMAKE_${LANGUAGE}_COMPILER_ID AND CMAKE_${LANGUAGE}_COMPILER_ID MATCHES "MSVC" )
 
     set( PZY_COMPILER "mscv" )
-    set( PZY_COMPILER_VERSION ${CMAKE_C_COMPILER_VERSION} )
+    set( PZY_COMPILER_VERSION ${CMAKE_${LANGUAGE}_COMPILER_VERSION} )
 
-    if( MSVC_C_ARCHITECTURE_ID STREQUAL "X86" )
+    if( MSVC_${LANGUAGE}_ARCHITECTURE_ID STREQUAL "X86" )
         set( PZY_ARCH 32 )
         set( PZY_COMPILER_ARCH "x86" )
-    elseif( MSVC_C_ARCHITECTURE_ID STREQUAL "x64" )
-        set( PZY_ARCH 64 )
-        set( PZY_COMPILER_ARCH "x64" )
-    endif()
-
-    if( MSVC_VERSION EQUAL 1400 )
-        set( PZY_COMPILER_NAME "vs2005" )
-    elseif( MSVC_VERSION EQUAL 1500 )
-        set( PZY_COMPILER_NAME "vs2008" )
-    elseif( MSVC_VERSION EQUAL 1600 )
-        set( PZY_COMPILER_NAME "vs2010" )
-    elseif( MSVC_VERSION EQUAL 1700 )
-        set( PZY_COMPILER_NAME "vs2012" )
-    elseif( MSVC_VERSION EQUAL 1800 )
-        set( PZY_COMPILER_NAME "vs2013" )
-    elseif( MSVC_VERSION EQUAL 1900 )
-        set( PZY_COMPILER_NAME "vs2015" )
-    endif()
-
-elseif( CMAKE_CXX_COMPILER_ID AND CMAKE_CXX_COMPILER_ID MATCHES "MSVC" )
-
-    set( PZY_COMPILER "msvc" )
-    set( PZY_COMPILER_VERSION ${CMAKE_CXX_COMPILER_VERSION} )
-
-    if( MSVC_CXX_ARCHITECTURE_ID STREQUAL "X86" )
-        set( PZY_ARCH 32 )
-        set( PZY_COMPILER_ARCH "x86" )
-    elseif( MSVC_CXX_ARCHITECTURE_ID STREQUAL "x64" )
+    elseif( MSVC_${LANGUAGE}_ARCHITECTURE_ID STREQUAL "x64" )
         set( PZY_ARCH 64 )
         set( PZY_COMPILER_ARCH "x64" )
     endif()
